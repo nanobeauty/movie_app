11dc37c0006d96ff9749f3157e4f4034966ba62e
Nano_Macbook <nanobeauty@icloud.com>
2018-09-16 02:49:46 +0700

    <Validating with PropTypes> Warning: Each child in an array or iterator should have a unique "key" prop.
    Check the render method of `App`. See https://fb.me/react-warning-keys for more information.
        in Movie (at App.js:29)
        in App (at src/index.js:7)
    
    ==>  map 함수에  index라는 변수를 추가해서 key prop에 대응해 준다.
    
    Compiled with warnings.
    
    ./src/Movie.js
      Line 7:  React.PropTypes is deprecated since React 15.5.0, use the npm module prop-types instead  react/no-deprecated
    
    Search for the keywords to learn more about each warning.
    To ignore, add // eslint-disable-next-line to the line before.
    
    ^C
    nanobeautyui-MacBook:movie_app nanobeauty$ yarn add prop-types

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
 class Movie extends Component {
+
+    static propTypes = {
+      title: PropTypes.string.isRequired,
+      poster: PropTypes.string.isRequired
+    }
+
     render() {
       return (
         <div className="Movie">
@@ -13,6 +20,11 @@ class Movie extends Component {
   }
 
   class MoviePoster extends Component {
+
+    static propTypes = {
+      posterVAL: PropTypes.string.isRequired
+    }
+
     render() {
       console.log(this.props);  //posterVAL로 이름 변경해서 구조를 더 명확히 하고 props로 posterVAL 확인
       return (
