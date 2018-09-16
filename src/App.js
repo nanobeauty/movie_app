bd9b2469c8ddc35489450a943bfc5f49b3e91375
Nano_Macbook <nanobeauty@icloud.com>
2018-09-16 21:16:18 +0700

    <Practicing this.setState()> …this.state.movie, 원래 리스트는 그냥 그대로 놔두고 다른 걸 추가한다는 의미

--- src/App.js
+++ src/App.js
@@ -2,47 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "http://www.gstatic.com/tv/thumb/v22vodart/22804/p22804_v_v8_as.jpg"
-  },
-  {
-    title: "Full Metal Jakcek",
-    poster: "http://www.gstatic.com/tv/thumb/v22vodart/10114/p10114_v_v8_ab.jpg"
-  },
-  {
-    title: "OldBoy",
-    poster: "http://www.gstatic.com/tv/thumb/v22vodart/35948/p35948_v_v8_ab.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://www.starwarsnewsnet.com/wp-content/uploads/2016/04/star-wars-the-force-awakens-the-saga-tribute-trailer-extended-722634.jpg"
-  }
-]
-
 class App extends Component {
 
   // Render: ComponentWillMount() -> render() -> componenentDidMount()
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
   state = {
-    greeting: 'Hello'
+    movies: [
+      {
+        title: "Matrix",
+        poster: "http://www.gstatic.com/tv/thumb/v22vodart/22804/p22804_v_v8_as.jpg"
+      },
+      {
+        title: "Full Metal Jakcek",
+        poster: "http://www.gstatic.com/tv/thumb/v22vodart/10114/p10114_v_v8_ab.jpg"
+      },
+      {
+        title: "OldBoy",
+        poster: "http://www.gstatic.com/tv/thumb/v22vodart/35948/p35948_v_v8_ab.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster: "https://www.starwarsnewsnet.com/wp-content/uploads/2016/04/star-wars-the-force-awakens-the-saga-tribute-trailer-extended-722634.jpg"
+      }
+    ]
   }
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        greeting: 'Hello Again'
+        movies: [
+          ...this.state.movies,
+          {
+            title: "trainspotting",
+            poster: "http://www.gstatic.com/tv/thumb/v22vodart/35948/p35948_v_v8_ab.jpg"
+          }
+        ]
       })
-    }, 2000)
+    }, 3000)
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
             return <Movie title={movie.title} poster={movie.poster} key={index} />
         })
                   /* 위와 같은 내용임.
