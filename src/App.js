b28a0f68b2e48183576d081bb4963b6d8e003436
Nano_Macbook <nanobeauty@icloud.com>
2018-09-18 02:08:26 +0700

    <Loading States> 데이터 없이 컴포넌트가 로딩을 하고 - Render 데이터를 위해 API를 불러서 - ComponentDidMount API가 데이터를 주면 - setTimeOut - this.setState (state가 변경) 컴포넌트 state를 업데이트.  - Render (API콜을 타임아웃 기능으로 유사하게 구현)
    {this.state.movies ? this._renderMovies() : 'Loading' }  참이면 전자, 거짓이면 후자
    
    arrow 함수
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions

--- src/App.js
+++ src/App.js
@@ -7,32 +7,28 @@ class App extends Component {
   // Render: ComponentWillMount() -> render() -> componenentDidMount()
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
-    movies: [
-      {
-        title: "Matrix",
-        poster: "http://www.gstatic.com/tv/thumb/v22vodart/22804/p22804_v_v8_as.jpg"
-      },
-      {
-        title: "Full Metal Jakcek",
-        poster: "http://www.gstatic.com/tv/thumb/v22vodart/10114/p10114_v_v8_ab.jpg"
-      },
-      {
-        title: "OldBoy",
-        poster: "http://www.gstatic.com/tv/thumb/v22vodart/35948/p35948_v_v8_ab.jpg"
-      },
-      {
-        title: "Star Wars",
-        poster: "https://www.starwarsnewsnet.com/wp-content/uploads/2016/04/star-wars-the-force-awakens-the-saga-tribute-trailer-extended-722634.jpg"
-      }
-    ]
-  }
+  state = {}
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            title: "Matrix",
+            poster: "http://www.gstatic.com/tv/thumb/v22vodart/22804/p22804_v_v8_as.jpg"
+          },
+          {
+            title: "Full Metal Jakcek",
+            poster: "http://www.gstatic.com/tv/thumb/v22vodart/10114/p10114_v_v8_ab.jpg"
+          },
+          {
+            title: "OldBoy",
+            poster: "http://www.gstatic.com/tv/thumb/v22vodart/35948/p35948_v_v8_ab.jpg"
+          },
+          {
+            title: "Star Wars",
+            poster: "https://www.starwarsnewsnet.com/wp-content/uploads/2016/04/star-wars-the-force-awakens-the-saga-tribute-trailer-extended-722634.jpg"
+          },
           {
             title: "trainspotting",
             poster: "http://www.gstatic.com/tv/thumb/v22vodart/35948/p35948_v_v8_ab.jpg"
@@ -42,21 +38,17 @@ class App extends Component {
     }, 3000)
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies;
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-            return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })
-                  /* 위와 같은 내용임.
-                  {[
-                  <Movie title={movieTitle[0]} poster={movieImage[0]} />
-                  <Movie title={movieTitle[1]} poster={movieImage[1]} />
-                  <Movie title={movieTitle[2]} poster={movieImage[2]} />
-                  <Movie title={movieTitle[3]} poster={movieImage[3]} />
-                  ]} 
-                  */
-        }
+        {this.state.movies ? this._renderMovies() : 'Loading' }
       </div>
     );
   }
