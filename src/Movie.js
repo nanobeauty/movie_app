8d564009cb342bd4c8c50524889b17e9940b940d
Nano_Macbook <nanobeauty@icloud.com>
2018-09-18 17:53:06 +0700

    <Smart VS Dumb Components> Props만 있는 경우 - stateless funcitonal 컴포넌트 ( Dumb Component ) 맨 위의 import {Component}에서 {Component}는 필요 없어서 삭제.

--- src/Movie.js
+++ src/Movie.js
@@ -1,38 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
 
-class Movie extends Component {
+// class Movie extends Component {
+//     static propTypes = {
+//       title: PropTypes.string.isRequired,
+//       poster: PropTypes.string.isRequired
+//     }
+//     render() {
+//       return (
+//         <div className="Movie">
+//             <MoviePoster posterVAL={this.props.poster}/>
+//             <h1>{this.props.title}</h1>
+//         </div>
+//       );
+//     }
+//   }
 
-    static propTypes = {
-      title: PropTypes.string.isRequired,
-      poster: PropTypes.string.isRequired
-    }
-
-    render() {
-      return (
-        <div className="Movie">
-            <MoviePoster posterVAL={this.props.poster}/>
-            <h1>{this.props.title}</h1>
-        </div>
-      );
-    }
+  function Movie({title, poster}){
+    return (
+      <div>
+        <MoviePoster posterVAL={poster}/>
+        <h1>{title}</h1>
+      </div>
+    )
   }
 
-  class MoviePoster extends Component {
+  Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+  }
 
-    static propTypes = {
-      posterVAL: PropTypes.string.isRequired
-    }
+  // class MoviePoster extends Component {
+  //   static propTypes = {
+  //     posterVAL: PropTypes.string.isRequired
+  //   }
+  //   render() {
+  //     console.log(this.props);  //posterVAL로 이름 변경해서 구조를 더 명확히 하고 props로 posterVAL 확인
+  //     return (
+  //       <div className="MoviePoster">
+  //         <img src={this.props.posterVAL} alt= "" />
+  //       </div>
+  //     );
+  //   }
+  // }
 
-    render() {
-      console.log(this.props);  //posterVAL로 이름 변경해서 구조를 더 명확히 하고 props로 posterVAL 확인
-      return (
-        <div className="MoviePoster">
-          <img src={this.props.posterVAL} alt= "" />
-        </div>
-      );
-    }
+  function MoviePoster({posterVAL}){
+    return (
+      <img src={posterVAL} alt= "" />
+    )
+  }
+
+  MoviePoster.propTypes = {
+    posterVAL: PropTypes.string.isRequired
   }
   
   export default Movie;
