1933f86eda9532d696eb7ffc9d2acae5794df755
Nano_Macbook <nanobeauty@icloud.com>
2018-09-19 10:04:24 +0700

    <Promise>  Promise는 성공과 실패의 두 경우의 수에 대한 시나리오 (성공 - then으로 받는다.  실패 - catch로 받아 후속작업을 추가해준다)     fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')     .then(potato => potato.json())     .then(json => console.log(json))     .catch(err => console.log(err))

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
 
   componentDidMount(){
     fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
